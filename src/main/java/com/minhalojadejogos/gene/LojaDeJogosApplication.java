32645f945b0df98f2acd3f76541a87721af65074
Luiz Alberto Melin <luizmelin34@gmail.com>
2022-01-24 09:19:46 -0300

    começo do projeto

---
+++ src/main/java/com/minhalojadejogos/gene/LojaDeJogosApplication.java
@@ -0,0 +1,13 @@
+package com.minhalojadejogos.gene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeJogosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeJogosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minhalojadejogos/gene/LojaDeJogosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhalojadejogos.gene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaDeJogosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
