6606ef0ee9b5a2905d4db22f8440c41cc317d809
Daniel <kr.danielreis@gmail.com>
2022-01-31 12:44:55 -0300

    @DanielSantosReis [ADD UserService e UsuarioController]

---
+++ src/main/java/com/minhalojadejogos/gene/controller/UsuarioController.java
@@ -0,0 +1,39 @@
+package com.minhalojadejogos.gene.controller;
+
+	
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.minhalojadejogos.gene.model.UserLogin;
+import com.minhalojadejogos.gene.model.Usuario;
+import com.minhalojadejogos.gene.service.UserService;
+
+	@RestController
+	@RequestMapping("/usuarios")
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
+	public class UsuarioController {
+		
+		@Autowired
+		private UserService usuarioService;
+		
+		@PostMapping("/logar")
+		public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
+			return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
+					.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+		}
+		
+		@PostMapping("/cadastrar")
+		public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
+			return ResponseEntity.status(HttpStatus.CREATED)
+					.body(usuarioService.cadastrarUsuario(usuario));
+	}
+}
+
+
---
+++ src/main/java/com/minhalojadejogos/gene/service/UserService.java
@@ -0,0 +1,49 @@
+package com.minhalojadejogos.gene.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.minhalojadejogos.gene.model.UserLogin;
+import com.minhalojadejogos.gene.model.Usuario;
+import com.minhalojadejogos.gene.repository.UsuarioRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
+	public Usuario cadastrarUsuario(Usuario usuario) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		String senhaEncoder = encoder.encode(usuario.getSenha());
+		usuario.setSenha(senhaEncoder);
+		return repository.save(usuario);
+	}
+	
+	public Optional<UserLogin> Logar(Optional<UserLogin> user) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		
+		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+		
+		if(usuario.isPresent()) {
+			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())) {
+				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
+				byte [] encodeAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String (encodeAuth);
+				
+				user.get().setToken(authHeader);
+				user.get().setNome(usuario.get().getNome());
+				
+				return user;
+			}
+		}
+		
+		return null;		
+		
+	}
+}
