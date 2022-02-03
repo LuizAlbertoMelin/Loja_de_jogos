a50fb0f38c3e4740346efd489c25029b464cca28
Daniel <kr.danielreis@gmail.com>
2022-02-03 18:52:00 -0300

    @DanielSantosReis [ ADD Applications-DEV and PROD. ]

--- src/main/java/com/minhalojadejogos/gene/seguranca/BasicSecurityConfig.java
+++ src/main/java/com/minhalojadejogos/gene/seguranca/BasicSecurityConfig.java
@@ -19,6 +19,9 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
+		
+		auth.inMemoryAuthentication().withUser("root").password(passawordEncoder().encode("root"))
+		 .authorities("ROLE_ADMIN");
 	}
 	
 	@Bean
